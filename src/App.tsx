65029af99bb676c05d44ad2d58ef753877ba2154
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-02-04 21:42:15 +0400

    bootstrap add

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router/router'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
+import './style/global.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+export const App = () => {
+
+  return <Provider store={store}>
+      <RouterProvider router={router}/>
+  </Provider>
+
 }
-
-export default App;
