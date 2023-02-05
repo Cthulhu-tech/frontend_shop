cc50a0915a5ff30936af2f185cba3fc5c30362bf
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-02-05 20:53:59 +0400

    protected router create

--- src/components/protected/notProtected.tsx
+++ src/components/protected/notProtected.tsx
@@ -2,9 +2,9 @@ import { JwtDecode } from '../../utils/decodeJWT'
 import { Navigate } from 'react-router-dom'
 import { useState } from 'react'
 
-export const Protected = (props: {children: React.ReactNode}) => {
+export const NotProtected = (props: {children: React.ReactNode}) => {
 
     const [userId] = useState(JwtDecode())
 
-    return userId ? <>{props.children}</> : <Navigate to="/" replace />
+    return !userId ? <>{props.children}</> : <Navigate to="/" replace />
 }
---
+++ src/components/protected/refreshToken.tsx
@@ -0,0 +1,21 @@
+import { updateToken } from '../../redux/store/token'
+import { Loading } from '../loading/loading'
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import axios from 'axios'
+
+export const RefreshToken = (props: {children: React.ReactNode}) => {
+
+    const dispatch = useDispatch()
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        axios.post(process.env.REACT_APP_SERVER + 'token/refresh', null, { withCredentials: true })
+        .then((data) => {
+            if(data.data.accesstoken) dispatch(updateToken(data.data.accesstoken))
+        })
+        .finally(() => setLoading(false))
+    },[])
+
+    return loading ? <Loading/> : <>{props.children}</>
+}
