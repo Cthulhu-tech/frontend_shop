6dd94c9d87d55a2b2cf8a99a59bba95747a9ab3d
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-02-08 01:25:48 +0400

    validator create add

---
+++ src/utils/validationCreate.tsx
@@ -0,0 +1,14 @@
+import { ObjectValidation } from '../hook/type'
+
+export const validationCreate = (data: ObjectValidation) => {
+
+    const errors:ObjectValidation = {}
+
+    if(!data['title']) errors['title'] = 'Заголовок обязателен для заполнения'
+    if(!data['description']) errors['description']  = 'Описание обязательно для заполнения'
+    if(!data['city']) errors['city']  = 'Город обязательно для заполнения'
+
+    if(Object.keys(errors).length > 0) return {check: false, error: errors}
+
+    return {check: true, error: errors}
+}
