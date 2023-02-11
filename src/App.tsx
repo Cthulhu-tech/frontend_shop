9c4101169e81cb2b66a8e2c639fad05545e5faf4
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-02-11 08:32:34 +0400

    update style

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import { Provider } from 'react-redux'
 import { store } from './redux/store'
 import './style/global.css'
 
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-
 export const App = () => {
 
   return <Provider store={store}>
