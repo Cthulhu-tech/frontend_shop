d3f54cd4431e748d0ac0ef11f5a4a90251c5454b
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-02-11 08:31:58 +0400

    update auth

--- src/view/auth/auth.tsx
+++ src/view/auth/auth.tsx
@@ -2,10 +2,10 @@ import { NavLink, Outlet } from 'react-router-dom'
 
 export const Auth = () => {
 
-    return <section className='container mt-5 pt-5 mb-5'>
-        <div className='row mb-5 mt-5 d-flex justify-content-center'>
-            <NavLink to='/auth' className='col-2 btn-primary text-center'>Вход</NavLink>
-            <NavLink to='/auth/registration' className='col-2 btn-primary text-center'>Регистрация</NavLink>
+    return <section className="w-full max-w-xl m-auto">
+        <div className='mb-5 mt-5 flex'>
+            <NavLink to='/auth' className='m-5 text-sm text-blue-600 font-bold hover:text-blue-900 duration-200 cursor-pointer active active'>Вход</NavLink>
+            <NavLink to='/auth/registration' className='m-5 text-sm text-blue-600 font-bold hover:text-blue-900 duration-200 cursor-pointer active active'>Регистрация</NavLink>
         </div>
         <Outlet/>
     </section>
