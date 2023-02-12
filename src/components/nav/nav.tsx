a38b12f337d151eab0174bd1aafe2a1c13f828b3
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-02-12 09:54:02 +0400

    navigation update

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -14,6 +14,7 @@ export const Nav = () => {
     return <nav className="sticky top-0 flex justify-between items-center bg-slate-100 shadow-lg h-12 md:flex">
         <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
             <ul className="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:items-center lg:w-auto lg:space-x-6">
+                <NavLink className="text-sm text-blue-600 font-bold flex items-center hover:text-blue-900 duration-200 cursor-pointer active" to='/'>Объявления</NavLink>
                 <NavLink className="text-sm text-blue-600 font-bold flex items-center hover:text-blue-900 duration-200 cursor-pointer active" to='/delayed'><Box2Heart className="mr-2"/> Избранное</NavLink>
                 {!access 
                 ? <NavLink className="text-sm text-blue-600 font-bold hover:text-blue-900 duration-200 cursor-pointer active" to='/auth'>Вход и регистрация</NavLink> 
