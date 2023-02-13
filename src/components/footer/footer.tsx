c29a0e6e3748dbe0eb1ce73ebac37206ae9a43de
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-02-13 14:40:27 +0400

    update

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,9 @@
 export const Footer = () => {
 
-    return <footer>
-        footer
+    return  <footer className="text-center bg-gray-800 text-dark p-5">
+        <div className="text-center p-4">
+            © 2021 Copyright:
+            <a className="text-whitehite m-5" href="https://github.com/Cthulhu-tech">Cthulhu-tech</a>
+        </div>
     </footer>
 }
