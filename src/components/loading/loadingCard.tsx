6cc1f793f3e6a4dbd81685b279e3c849e1203943
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-02-13 14:40:56 +0400

    loading update

--- src/components/loading/loadingCard.tsx
+++ src/components/loading/loadingCard.tsx
@@ -1,5 +1,5 @@
 export const LoadingCard = () => {
-    return  <div className="md:w-1/3">
+    return  <div className="md:w-1/3 m-auto">
         <div className="h-full border-2 border-gray-200 rounded-lg overflow-hidden bg-white">
         <div className="lg:h-48 bg-gray-400 md:h-96 w-full object-cover object-center"></div>
             <div className="p-6">
