26985ffab8f63288c8159f40d2bb8709f49922e6
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-02-13 14:41:56 +0400

    update

--- src/components/selectCategory/select.tsx
+++ src/components/selectCategory/select.tsx
@@ -1,16 +1,19 @@
 import { updateCategory } from '../../redux/store/category'
 import { useSelector, useDispatch } from 'react-redux'
 import { ReduxStore } from '../../redux/type'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export const Select = () => {
 
     const dispatch = useDispatch()
     const [top, setTop] = useState<string>()
+    const categoryId = useSelector((store: ReduxStore) => store.CategoryStore)
     const changeSelectCategoryTop = (event: React.ChangeEvent<HTMLSelectElement>) => setTop(event.currentTarget.value)
-    const changeSelectCategorySub = (event: React.ChangeEvent<HTMLSelectElement>) => dispatch(updateCategory(event.currentTarget.value))
+    const changeSelectCategorySub = (event: React.ChangeEvent<HTMLSelectElement> | React.MouseEvent<HTMLOptionElement, MouseEvent>) => dispatch(updateCategory(event.currentTarget.value))
     
     const categories = useSelector((store: ReduxStore) => store.CategoriesStore)
+
+    useEffect(() => {}, [top, categories, categoryId])
 
     return  <>
             <div className="m-5">
@@ -23,7 +26,7 @@ export const Select = () => {
             <div className="m-5">
                 <select className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" onChange={changeSelectCategorySub}>
                     {categories.map((category) =>
-                        top === category.name && category.sub.map((sub) => <option value={sub.id} key={sub.id}>{sub.mpath}</option>)
+                        top === category.name && category.sub.map((sub) => <option onClick={changeSelectCategorySub} value={sub.category_id} key={sub.category_id}>{sub.category_mpath}</option>)
                     )}
                 </select>
             </div>
