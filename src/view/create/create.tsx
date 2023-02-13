eab73f904c80aeca365e69e9d3c4ebdf73a5d6bb
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-02-13 14:43:23 +0400

    update

--- src/view/create/create.tsx
+++ src/view/create/create.tsx
@@ -13,15 +13,15 @@ const ImageUploadComponent = memo(ImageUpload)
 
 export const Create = () => {
 
-    const formRef = useRef<HTMLFormElement>(null)
     const dispatch = useDispatch()
+    const formRef = useRef<HTMLFormElement>(null)
     const token = useSelector((store: ReduxStore) => store.TokenStore)
     const { change, submit, errors } = useForm(validationCreate, 'product', true)
 
     useEffect(() => {
         if(token.access) axios.get(process.env.REACT_APP_SERVER + 'categories')
         .then((data) => dispatch(updateCategories(data.data)))
-    }, [])
+    }, [token.access, dispatch])
 
     return <div className="w-full max-w-xl m-auto">
         <form ref={formRef} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={submit}>
@@ -47,13 +47,26 @@ export const Create = () => {
                 <input type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Улица" name="street" onChange={change} />
             </div>
             <div className="mb-4">
-                <input type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Доставка" name="delivery" onChange={change} />
+                <div className="m-5">
+                    <select name="delivery" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" onChange={change}>
+                        <option value="0">Без доставки</option>
+                        <option value="1">Доставка</option>
+                    </select>
+                </div>
             </div>
             <SelectComponent/>
+            {errors && errors.categoryId && <p className="text-red-500 text-xs italic mt-2 mb-4">
+                {errors.categoryId}
+            </p>}
             <div className="mb-4">
                 <input type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Цена" name="price" onChange={change} />
             </div>
             <ImageUploadComponent/>
+            <div className="mb-4">
+                {errors && errors.serverNotError && <small className="text-green-500 text-xs italic mt-2">
+                    {errors.serverNotError}
+                </small>}
+            </div>
             <div className="mb-4">
                 <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Создать объявление</button>
             </div>
